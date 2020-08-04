aabc564c39e2e8f62b39c0efaf73350972d76f09
Anwar Ulhaq <mailtoanwar@gmail.com>
2020-08-04 04:06:15 +0300

    Added/Modified console outputs.

--- Independent_Tasks/src/Multiple_Threads.java
+++ Independent_Tasks/src/Multiple_Threads.java
@@ -3,27 +3,29 @@ import java.util.concurrent.TimeUnit;
 public class Multiple_Threads {
 
     public static void main(String[] args) throws InterruptedException {
+
         Runnable taskOne = () -> {
             for ( int i = 0; i <=3 ; i++  ){
-                System.out.println("Hello from " + Thread.currentThread().getName());
+                System.out.println("Hello from Task 1");
                 try {
                     TimeUnit.MILLISECONDS.sleep(1000);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
             }
-
+            System.out.println("Bye Bye from Task 1");
         };
 
         Runnable taskTwo = () -> {
             for ( int i = 1; i <=3 ; i++  ){
-                System.out.println("Hello from " + Thread.currentThread().getName());
+                System.out.println("Hello from  Task 2");
                 try {
                     TimeUnit.MILLISECONDS.sleep(1000);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
             }
+            System.out.println("Bye Bye from Task 2");
         };
         Thread taskOneThread = new Thread( taskOne, "Task one Thread" );
         Thread taskTwoThread = new Thread( taskTwo, "Task two Thread" );
@@ -31,7 +33,6 @@ public class Multiple_Threads {
         taskOneThread.start();
         taskTwoThread.start();
 
-        // Wait until threads are finished
         taskOneThread.join();
         taskTwoThread.join();
 
